48b4a00d3cd76f215bf848ed7276f68ff017e930
a0a0cjv <a0a0cjv@homeoffice.wal-mart.com>
2021-11-21 17:23:13 +0530

    golang practice

---
+++ file_code.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+type User struct {
+	Name       string
+	Occupation string
+	Born       string
+	Fav_food   []string
+}
+
+type fav_foods interface {
+	// Methods
+	Get_fav_food() string
+}
+
+func (U User) Get_fav_food() string {
+	// fmt.Println(U)
+	return U.Fav_food[0] + "," + U.Fav_food[1]
+}
+
+func fetch_fav_food(My_fav fav_foods) {
+	fmt.Println(My_fav.Get_fav_food())
+
+}
+
+func main() {
+	filename, err := os.Open("data.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := ioutil.ReadAll(filename)
+	defer filename.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var p []User
+	jsonErr := json.Unmarshal([]byte(data), &p)
+
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+	var listinterface fav_foods
+	for _, b := range p {
+		// fmt.Println(b)
+		// fmt.Println(b.type())
+		listinterface = &b
+		fetch_fav_food(listinterface)
+	}
+}
